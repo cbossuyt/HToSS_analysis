7421ee081baa6c8e7cc9c6003db8212429a08d91
davidcarbonis <alexander.david.morton@cern.ch>
2020-03-19 02:17:26 +0000

    Debugged for ICL and VUB operations

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 # Point LHAPDF to pdf sets
-export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc8-opt/share/LHAPDF:/cvmfs/cms.cern.ch/lhapdf/pdfsets/6.2.1/
+export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/views/LCG_96/x86_64-slc6-gcc8-opt/share/LHAPDF:/cvmfs/cms.cern.ch/lhapdf/pdfsets/6.2.1/
 
 export TQZ_TOOLS_PATH='.'
 
 # Enable newrt gcc and python
 #source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_95 x86_64-slc6-gcc8-opt 
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc8-opt/setup.sh 
+source /cvmfs/sft.cern.ch/lcg/views/LCG_96/x86_64-slc6-gcc8-opt/setup.sh 
