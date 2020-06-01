e6ddecab0c731b0bc555f3fc67ee1f1fd4b1d16d
davidcarbonis <alexander.david.morton@cern.ch>
2020-06-01 13:18:21 +0100

    Added human-readable output

--- src/genLevelAnalysis.cxx
+++ src/genLevelAnalysis.cxx
@@ -19,6 +19,7 @@
 #include <vector>
 
 std::string pythiaStatus (int status);
+std::string pdgId (int status);
 
 int main(int argc, char* argv[])
 {
@@ -124,7 +125,7 @@ int main(int argc, char* argv[])
             std::cout << "nGenPar: " << lEvent->nGenPar << std::endl;
             std::cout << "pdgId / mother / status: " << std::endl;
             for (Int_t k{0}; k < lEvent->nGenPar; k++) {
-                std::cout << lEvent->genParId[k] << " / " << lEvent->genParMotherId[k] << " / " << pythiaStatus( lEvent->genParStatus[k] ) << std::endl;
+                std::cout << pdgId( lEvent->genParId[k] ) << " / " << pdgId( lEvent->genParMotherId[k] ) << " / " << pythiaStatus( lEvent->genParStatus[k] ) << std::endl;
             }
             std::cout << std::endl;
 
@@ -155,225 +156,164 @@ std::string pythiaStatus (int status) {
    switch (status) {
        case 11 : message += " : beam particles - the event as a whole";
            break;
-   }
-   switch (status) {
        case 12 : message += " : beam particles - incoming beam";
            break;
-   }
-   switch (status) {
        case 13 : message += " : beam particles - incoming beam-inside-beam (e.g. gamma inside e)";
            break;
-   }
-   switch (status) {
        case 14 : message += " : beam particles - outgoing elastically scattered";
            break;
-   }
-   switch (status) {
        case 15 : message += " : beam particles - outgoing diffractively scattered";
            break;
-   }
-   switch (status) {
        case 21 : message += " : particles of the hardest subprocess - incoming";
            break;
-   }
-   switch (status) {
        case 22 : message += " : particles of the hardest subprocess - intermediate (intended to have preserved mass)";
            break;
-   }
-   switch (status) {
        case 23 : message += " : particles of the hardest subprocess - outgoing";
            break;
-   }
-   switch (status) {
        case 24 : message += " : particles of the hardest subprocess - outgoing, nonperturbatively kicked out in diffraction";
            break;
-   }
-   switch (status) {
        case 31 : message += " : particles of subsequent subprocesses - incoming";
            break;
-   }
-   switch (status) {
        case 32 : message += " : particles of subsequent subprocesses - intermediate (intended to have preserved mass)";
            break;
-   }
-   switch (status) {
        case 33 : message += " : particles of subsequent subprocesses - outgoing";
            break;
-   }
-   switch (status) {
        case 34 : message += " : particles of subsequent subprocesses - incoming that has already scattered";
            break;
-   }
-   switch (status) {
        case 41 : message += " : particles produced by initial-state-showers - incoming on spacelike main branch";
            break;
-   }
-   switch (status) {
        case 42 : message += " : particles produced by initial-state-showers - incoming copy of recoiler";
            break;
-   }
-   switch (status) {
        case 43 : message += " : particles produced by initial-state-showers - outgoing produced by a branching";
            break;
-   }
-   switch (status) {
        case 44 : message += " : particles produced by initial-state-showers - outgoing shifted by a branching";
            break;
-   }
-   switch (status) {
        case 45 : message += " : particles produced by initial-state-showers - incoming rescattered parton, with changed kinematics owing to ISR in the mother system (cf. status 34)";
            break;
-   }
-   switch (status) {
        case 46 : message += " : particles produced by initial-state-showers - incoming copy of recoiler when this is a rescattered parton (cf. status 42)";
            break;
-   }
-   switch (status) {
        case 47 : message += " : particles produced by initial-state-showers - a W or Z gauge boson produced in the shower evolution";
            break;
-   }
-   switch (status) {
        case 51 : message += " : particles produced by final-state-showers - outgoing produced by parton branching";
            break;
-   }
-   switch (status) {
        case 52 : message += " : particles produced by final-state-showers - outgoing copy of recoiler, with changed momentum";
            break;
-   }
-   switch (status) {
        case 53 : message += " : particles produced by final-state-showers - copy of recoiler when this is incoming parton, with changed momentum";
            break;
-   }
-   switch (status) {
        case 54 : message += " : particles produced by final-state-showers - copy of a recoiler, when in the initial state of a different system from the radiator";
            break;
-   }
-   switch (status) {
        case 55 : message += " : particles produced by final-state-showers - copy of a recoiler, when in the final state of a different system from the radiator";
            break;
-   }
-   switch (status) {
        case 56 : message += " : particles produced by final-state-showers - a W or Z gauge boson produced in a shower branching (special case of 51)";
            break;
-   }
-   switch (status) {
        case 61 : message += " : particles produced by beam-remnant treatment - incoming subprocess particle with primordial kT included";
            break;
-   }
-   switch (status) {
        case 62 : message += " : particles produced by beam-remnant treatment - outgoing subprocess particle with primordial kT included";
            break;
-   }
-   switch (status) {
        case 63 : message += " : particles produced by beam-remnant treatment - outgoing beam remnant";
            break;
-   }
-   switch (status) {
        case 71 : message += " : partons in preparation of hadronization process - copied partons to collect into contiguous colour singlet";
            break;
-   }
-   switch (status) {
        case 72 : message += " : partons in preparation of hadronization process - copied recoiling singlet when ministring collapses to one hadron and momentum has to be reshuffled";
            break;
-   }
-   switch (status) {
        case 73 : message += " : partons in preparation of hadronization process - combination of very nearby partons into one";
            break;
-   }
-   switch (status) {
        case 74 : message += " : partons in preparation of hadronization process - combination of two junction quarks (+ nearby gluons) to a diquark";
            break;
-   }
-   switch (status) {
        case 75 : message += " : partons in preparation of hadronization process - gluons split to decouple a junction-antijunction pair";
            break;
-   }
-   switch (status) {
        case 76 : message += " : partons in preparation of hadronization process - partons with momentum shuffled to decouple a junction-antijunction pair";
            break;
-   }
-   switch (status) {
        case 77 : message += " : partons in preparation of hadronization process - temporary opposing parton when fragmenting first two strings in to junction (should disappear again)";
            break;
-   }
-   switch (status) {
        case 78 : message += " : partons in preparation of hadronization process - temporary combined diquark end when fragmenting last string in to junction (should disappear again)";
            break;
-   }
-   switch (status) {
        case 81 : message += " : primary hadrons produced by hadronization process - from ministring into one hadron";
            break;
-   }
-   switch (status) {
        case 82 : message += " : primary hadrons produced by hadronization process - from ministring into two hadron";
            break;
-   }
-   switch (status) {
        case 83 : message += " : primary hadrons produced by hadronization process - from normal string (the difference between the two is technical, whether fragmented off from the top of the string system or from the bottom, useful for debug only)";
            break;
-   }
-   switch (status) {
        case 84 : message += " : primary hadrons produced by hadronization process - from normal string (the difference between the two is technical, whether fragmented off from the top of the string system or from the bottom, useful for debug only)";
            break;
-   }
-   switch (status) {
        case 85 : message += " : primary hadrons produced by hadronization process - primary produced hadrons in junction fragmentation of the first two string legs in to the junction, in order of treatment";
            break;
-   }
-   switch (status) {
        case 86 : message += " : primary hadrons produced by hadronization process - primary produced hadrons in junction fragmentation of the first two string legs in to the junction, in order of treatment";
            break;
-   }
-   switch (status) {
        case 91 : message += " : particles produced in decay process, or by Bose-Einstein effects - normal decay products";
            break;
-   }
-   switch (status) {
        case 92 : message += " : particles produced in decay process, or by Bose-Einstein effects - decay products after oscillation";
            break;
-   }
-   switch (status) {
        case 93 : message += " : particles produced in decay process, or by Bose-Einstein effects - decay handled by external program, normally or with oscillation";
            break;
-   }
-   switch (status) {
        case 94 : message += " : particles produced in decay process, or by Bose-Einstein effects - decay handled by external program, normally or with oscillation";
            break;
-   }
-   switch (status) {
        case 99 : message += " : particles produced in decay process, or by Bose-Einstein effects - particles with momenta shifted by Bose-Einstein effects";
            break;
-   }
-   switch (status) {
        case 101 : message += " : particles in the handling of R-hadron production and decay, i.e. long-lived (or stable) particles with very heavy flavour - when a string system contains two such long-lived particles, the system is split up by the production of a new q-qbar pair";
            break;
-   }
-   switch (status) {
        case 102 : message += " : particles in the handling of R-hadron production and decay, i.e. long-lived (or stable) particles with very heavy flavour - partons rearranged from the long-lived particle end to prepare for fragmentation from this end";
            break;
-   }
-   switch (status) {
        case 103 : message += " : particles in the handling of R-hadron production and decay, i.e. long-lived (or stable) particles with very heavy flavour - intermediate half-R-hadron ormed when a colour octet particle (like the gluino) has been fragmented on one side, but not yet on the other";
            break;
-   }
-   switch (status) {
        case 104 : message += " : particles in the handling of R-hadron production and decay, i.e. long-lived (or stable) particles with very heavy flavour - an R-hadron";
            break;
-   }
-   switch (status) {
        case 105 : message += " : particles in the handling of R-hadron production and decay, i.e. long-lived (or stable) particles with very heavy flavour - partons or particles formed together with the R-hadron during the fragmentation treatment";
            break;
-   }
-   switch (status) {
        case 106 : message += " : particles in the handling of R-hadron production and decay, i.e. long-lived (or stable) particles with very heavy flavour - subdivision of an R-hadron into its flavour content, with momentum split accordingly, in preparation of the decay of the heavy new particle, if it is unstable";
            break;
-   }
-   switch (status) {
        case 107 : message += " : particles in the handling of R-hadron production and decay, i.e. long-lived (or stable) particles with very heavy flavour - two temporary leftover gluons joined into one in the formation of a gluino-gluon R-hadron";
            break;
+       default: message += " : unknown status code ";
    }
-
-   if (status > 107 ) message += " :  : unknown status code ";
 
    return message;
 }
+
+std::string pdgId (int parId) {
+
+   std::string particle;
+   int id = std::abs(parId);
+
+   switch (id) {
+      case 1 : particle += "d"; break;
+      case 2 : particle += "u"; break;
+      case 3 : particle += "s"; break;
+      case 4 : particle += "c"; break;
+      case 5 : particle += "b"; break;
+      case 6 : particle += "t"; break;
+      case 7 : particle += "b\'"; break;
+      case 8 : particle += "t\t"; break;
+      case 9 : particle += "g"; break;
+
+      case 11 : particle += "e"; break;
+      case 12 : particle += "\u03BD_e"; break;
+      case 13 : particle += "mu"; break;
+      case 14 : particle += "\u03BD_mu"; break;
+      case 15 : particle += "\u03C4"; break;
+      case 16 : particle += "\u03BD_\u03C4"; break;
+      case 17 : particle += "\u03C4\'"; break;
+      case 18 : particle += "\u03BD_\u03C4\'"; break;
+
+      case 21 : particle += "g"; break;
+      case 22 : particle += "\u03B3"; break;
+      case 23 : particle += "Z0"; break;
+      case 24 : particle += "W"; break;
+      case 25 : particle += "h0"; break;
+
+      case 111 : particle += "\u03C00"; break;
+      case 211 : particle += "\u03C0"; break;
+
+      case 2212 : particle += "p"; break;
+      case 2112 : particle += "n"; break;
+
+      case 9000006 : particle += "S"; break;
+      default: particle += std::to_string(std::abs(parId));
+   }
+
+   if (parId == 211) particle += "+";
+   if (parId < 0) particle += "-";
+   return particle;
+
+}
+
