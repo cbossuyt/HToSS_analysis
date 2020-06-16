1d341552aca2f8a454884f8d06444bcb4bde0324
davidcarbonis <alexander.david.morton@cern.ch>
2020-06-16 17:28:44 +0100

    Helper script commit

---
+++ scripts/makeGenPlots.sh
@@ -0,0 +1,41 @@
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS1_ctauS1_Conf.yaml -o ggHZ_MS1_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS2_ctauS1_Conf.yaml -o ggHZ_MS2_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS40_ctauS1_Conf.yaml -o ggHZ_MS40_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS55_ctauS1_Conf.yaml -o ggHZ_MS55_ctauS1.root
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS1_ctauS10_Conf.yaml -o ggHZ_MS1_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS2_ctauS10_Conf.yaml -o ggHZ_MS2_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS40_ctauS10_Conf.yaml -o ggHZ_MS40_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS55_ctauS10_Conf.yaml -o ggHZ_MS55_ctauS10.root
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS1_ctauS100_Conf.yaml -o ggHZ_MS1_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS2_ctauS100_Conf.yaml -o ggHZ_MS2_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS40_ctauS100_Conf.yaml -o ggHZ_MS40_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS55_ctauS100_Conf.yaml -o ggHZ_MS55_ctauS100.root
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS1_ctauS1000_Conf.yaml -o ggHZ_MS1_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS2_ctauS1000_Conf.yaml -o ggHZ_MS2_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS40_ctauS1000_Conf.yaml -o ggHZ_MS40_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS55_ctauS1000_Conf.yaml -o ggHZ_MS55_ctauS1000.root
+
+###
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS1_ctauS1_Conf.yaml -o HZJ_MS1_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS2_ctauS1_Conf.yaml -o HZJ_MS2_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS40_ctauS1_Conf.yaml -o HZJ_MS40_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS55_ctauS1_Conf.yaml -o HZJ_MS55_ctauS1.root
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS1_ctauS10_Conf.yaml -o HZJ_MS1_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS2_ctauS10_Conf.yaml -o HZJ_MS2_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS40_ctauS10_Conf.yaml -o HZJ_MS40_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS55_ctauS10_Conf.yaml -o HZJ_MS55_ctauS10.root
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS1_ctauS100_Conf.yaml -o HZJ_MS1_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS2_ctauS100_Conf.yaml -o HZJ_MS2_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS40_ctauS100_Conf.yaml -o HZJ_MS40_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS55_ctauS100_Conf.yaml -o HZJ_MS55_ctauS100.root
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS1_ctauS1000_Conf.yaml -o HZJ_MS1_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS2_ctauS1000_Conf.yaml -o HZJ_MS2_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS40_ctauS1000_Conf.yaml -o HZJ_MS40_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS55_ctauS1000_Conf.yaml -o HZJ_MS55_ctauS1000.root
