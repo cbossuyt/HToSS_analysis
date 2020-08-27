1ce8484e313bdef343a9bfa75f94f76c94db653b
davidcarbonis <alexander.david.morton@cern.ch>
2020-08-27 18:07:10 +0100

    Updated file paths

--- scripts/makeGenPlots.sh
+++ scripts/makeGenPlots.sh
@@ -1,63 +1,63 @@
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS1_ctauS1_Conf.yaml -o ggHZ_MS1_ctauS1.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS2_ctauS1_Conf.yaml -o ggHZ_MS2_ctauS1.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS40_ctauS1_Conf.yaml -o ggHZ_MS40_ctauS1.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS55_ctauS1_Conf.yaml -o ggHZ_MS55_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS1_ctauS1_Conf.yaml -o ggHZ_Shadronic_MS1_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS2_ctauS1_Conf.yaml -o ggHZ_Shadronic_MS2_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS40_ctauS1_Conf.yaml -o ggHZ_Shadronic_MS40_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS55_ctauS1_Conf.yaml -o ggHZ_Shadronic_MS55_ctauS1.root
 
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS1_ctauS10_Conf.yaml -o ggHZ_MS1_ctauS10.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS2_ctauS10_Conf.yaml -o ggHZ_MS2_ctauS10.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS40_ctauS10_Conf.yaml -o ggHZ_MS40_ctauS10.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS55_ctauS10_Conf.yaml -o ggHZ_MS55_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS1_ctauS10_Conf.yaml -o ggHZ_Shadronic_MS1_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS2_ctauS10_Conf.yaml -o ggHZ_Shadronic_MS2_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS40_ctauS10_Conf.yaml -o ggHZ_Shadronic_MS40_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS55_ctauS10_Conf.yaml -o ggHZ_Shadronic_MS55_ctauS10.root
 
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS1_ctauS100_Conf.yaml -o ggHZ_MS1_ctauS100.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS2_ctauS100_Conf.yaml -o ggHZ_MS2_ctauS100.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS40_ctauS100_Conf.yaml -o ggHZ_MS40_ctauS100.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS55_ctauS100_Conf.yaml -o ggHZ_MS55_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS1_ctauS100_Conf.yaml -o ggHZ_Shadronic_MS1_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS2_ctauS100_Conf.yaml -o ggHZ_Shadronic_MS2_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS40_ctauS100_Conf.yaml -o ggHZ_Shadronic_MS40_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS55_ctauS100_Conf.yaml -o ggHZ_Shadronic_MS55_ctauS100.root
 
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS1_ctauS1000_Conf.yaml -o ggHZ_MS1_ctauS1000.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS2_ctauS1000_Conf.yaml -o ggHZ_MS2_ctauS1000.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS40_ctauS1000_Conf.yaml -o ggHZ_MS40_ctauS1000.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_MS55_ctauS1000_Conf.yaml -o ggHZ_MS55_ctauS1000.root
-
-###
-
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS1_ctauS1_Conf.yaml -o HZJ_MS1_ctauS1.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS2_ctauS1_Conf.yaml -o HZJ_MS2_ctauS1.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS40_ctauS1_Conf.yaml -o HZJ_MS40_ctauS1.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS55_ctauS1_Conf.yaml -o HZJ_MS55_ctauS1.root
-
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS1_ctauS10_Conf.yaml -o HZJ_MS1_ctauS10.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS2_ctauS10_Conf.yaml -o HZJ_MS2_ctauS10.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS40_ctauS10_Conf.yaml -o HZJ_MS40_ctauS10.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS55_ctauS10_Conf.yaml -o HZJ_MS55_ctauS10.root
-
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS1_ctauS100_Conf.yaml -o HZJ_MS1_ctauS100.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS2_ctauS100_Conf.yaml -o HZJ_MS2_ctauS100.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS40_ctauS100_Conf.yaml -o HZJ_MS40_ctauS100.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS55_ctauS100_Conf.yaml -o HZJ_MS55_ctauS100.root
-
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS1_ctauS1000_Conf.yaml -o HZJ_MS1_ctauS1000.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS2_ctauS1000_Conf.yaml -o HZJ_MS2_ctauS1000.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS40_ctauS1000_Conf.yaml -o HZJ_MS40_ctauS1000.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_MS55_ctauS1000_Conf.yaml -o HZJ_MS55_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS1_ctauS1000_Conf.yaml -o ggHZ_Shadronic_MS1_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS2_ctauS1000_Conf.yaml -o ggHZ_Shadronic_MS2_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS40_ctauS1000_Conf.yaml -o ggHZ_Shadronic_MS40_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggHZ_Shadronic_MS55_ctauS1000_Conf.yaml -o ggHZ_Shadronic_MS55_ctauS1000.root
 
 ###
 
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS1_ctauS1_Conf.yaml -o ggH_MS1_ctauS1.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS2_ctauS1_Conf.yaml -o ggH_MS2_ctauS1.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS40_ctauS1_Conf.yaml -o ggH_MS40_ctauS1.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS55_ctauS1_Conf.yaml -o ggH_MS55_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS1_ctauS1_Conf.yaml -o HZJ_Shadronic_MS1_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS2_ctauS1_Conf.yaml -o HZJ_Shadronic_MS2_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS40_ctauS1_Conf.yaml -o HZJ_Shadronic_MS40_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS55_ctauS1_Conf.yaml -o HZJ_Shadronic_MS55_ctauS1.root
 
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS1_ctauS10_Conf.yaml -o ggH_MS1_ctauS10.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS2_ctauS10_Conf.yaml -o ggH_MS2_ctauS10.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS40_ctauS10_Conf.yaml -o ggH_MS40_ctauS10.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS55_ctauS10_Conf.yaml -o ggH_MS55_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS1_ctauS10_Conf.yaml -o HZJ_Shadronic_MS1_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS2_ctauS10_Conf.yaml -o HZJ_Shadronic_MS2_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS40_ctauS10_Conf.yaml -o HZJ_Shadronic_MS40_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS55_ctauS10_Conf.yaml -o HZJ_Shadronic_MS55_ctauS10.root
 
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS1_ctauS100_Conf.yaml -o ggH_MS1_ctauS100.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS2_ctauS100_Conf.yaml -o ggH_MS2_ctauS100.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS40_ctauS100_Conf.yaml -o ggH_MS40_ctauS100.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS55_ctauS100_Conf.yaml -o ggH_MS55_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS1_ctauS100_Conf.yaml -o HZJ_Shadronic_MS1_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS2_ctauS100_Conf.yaml -o HZJ_Shadronic_MS2_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS40_ctauS100_Conf.yaml -o HZJ_Shadronic_MS40_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS55_ctauS100_Conf.yaml -o HZJ_Shadronic_MS55_ctauS100.root
 
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS1_ctauS1000_Conf.yaml -o ggH_MS1_ctauS1000.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS2_ctauS1000_Conf.yaml -o ggH_MS2_ctauS1000.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS40_ctauS1000_Conf.yaml -o ggH_MS40_ctauS1000.root
-./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_MS55_ctauS1000_Conf.yaml -o ggH_MS55_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS1_ctauS1000_Conf.yaml -o HZJ_Shadronic_MS1_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS2_ctauS1000_Conf.yaml -o HZJ_Shadronic_MS2_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS40_ctauS1000_Conf.yaml -o HZJ_Shadronic_MS40_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_HZJ_Shadronic_MS55_ctauS1000_Conf.yaml -o HZJ_Shadronic_MS55_ctauS1000.root
+
+###
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS1_ctauS1_Conf.yaml -o ggH_Shadronic_MS1_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS2_ctauS1_Conf.yaml -o ggH_Shadronic_MS2_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS40_ctauS1_Conf.yaml -o ggH_Shadronic_MS40_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS55_ctauS1_Conf.yaml -o ggH_Shadronic_MS55_ctauS1.root
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS1_ctauS10_Conf.yaml -o ggH_Shadronic_MS1_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS2_ctauS10_Conf.yaml -o ggH_Shadronic_MS2_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS40_ctauS10_Conf.yaml -o ggH_Shadronic_MS40_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS55_ctauS10_Conf.yaml -o ggH_Shadronic_MS55_ctauS10.root
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS1_ctauS100_Conf.yaml -o ggH_Shadronic_MS1_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS2_ctauS100_Conf.yaml -o ggH_Shadronic_MS2_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS40_ctauS100_Conf.yaml -o ggH_Shadronic_MS40_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS55_ctauS100_Conf.yaml -o ggH_Shadronic_MS55_ctauS100.root
+
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS1_ctauS1000_Conf.yaml -o ggH_Shadronic_MS1_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS2_ctauS1000_Conf.yaml -o ggH_Shadronic_MS2_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS40_ctauS1000_Conf.yaml -o ggH_Shadronic_MS40_ctauS1000.root
+./bin/genLevelAnalysis.exe -c configs/2017/triggerStudy_ggH_Shadronic_MS55_ctauS1000_Conf.yaml -o ggH_Shadronic_MS55_ctauS1000.root
