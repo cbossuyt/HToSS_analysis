abdbe1174f821d3798dbbc6a2266fe298243b76c
davidcarbonis <alexander.david.morton@cern.ch>
2020-09-24 18:42:08 +0200

    Update plot scripts

---
+++ scripts/makeGenPlots_SmuonHadronFiltered.sh
@@ -0,0 +1,10 @@
+./bin/genLevelAnalysis.exe -c configs/2017/ggH/ggH_MS1_ctauS1_Conf.yaml -o ggH_SmuonHadronFiltered_MS1_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/ggH/ggH_MS1_ctauS10_Conf.yaml -o ggH_SmuonHadronFiltered_MS1_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/ggH/ggH_MS1_ctauS100_Conf.yaml -o ggH_SmuonHadronFiltered_MS1_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/ggH/ggH_MS1_ctauS1000_Conf.yaml -o ggH_SmuonHadronFiltered_MS1_ctauS1000.root
+
+./bin/genLevelAnalysis.exe -c configs/2017/ggH/ggH_MS2_ctauS1_Conf.yaml -o ggH_SmuonHadronFiltered_MS2_ctauS1.root
+./bin/genLevelAnalysis.exe -c configs/2017/ggH/ggH_MS2_ctauS10_Conf.yaml -o ggH_SmuonHadronFiltered_MS2_ctauS10.root
+./bin/genLevelAnalysis.exe -c configs/2017/ggH/ggH_MS2_ctauS100_Conf.yaml -o ggH_SmuonHadronFiltered_MS2_ctauS100.root
+./bin/genLevelAnalysis.exe -c configs/2017/ggH/ggH_MS2_ctauS1000_Conf.yaml -o ggH_SmuonHadronFiltered_MS2_ctauS1000.root
+
